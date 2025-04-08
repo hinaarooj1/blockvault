202c8d319861a1b222ed15643eb32633c40e0185
A. Developers <hinaarooj978@gmail.com>
2025-04-09 04:45:59 +0500

    initialize project structure with basic files and configurations

---
+++ FE/src/utils/Constant.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Local server
+  // baseUrl: "http://localhost:4000/api/v1",
+  // Locala server
+  // Official server 
+  baseUrl: "https://blockvault.onrender.com/api/v1",
+  // Official server
+};
